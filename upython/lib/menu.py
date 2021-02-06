19c7ce439f4879f945d50109b3a56f5d3f6db60b
yazjack <yazjack@gmail.com>
2021-02-06 21:12:21 +0100

    class prototype for menu display

--- upython/lib/menu.py
+++ upython/lib/menu.py
@@ -2,37 +2,37 @@ import interrupts as itr
 import definitions as df
 from ST7735 import TFT
 from sysfont import sysfont
+left_margin = 15
 
-def mainMenuLayout():
-    df.tft.fill(TFT.BLACK)
-    left_margin = 15
-    h = 5
-    df.tft.text((left_margin, h), "MAIN MENU", TFT.GRAY, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    df.tft.text((left_margin, h), "Choose stock type", TFT.WHITE, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    df.tft.text((left_margin, h), "Choose stock size", TFT.WHITE, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    df.tft.text((left_margin, h), "Stock ammount", TFT.WHITE, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    df.tft.text((left_margin, h), "Valve o/c", TFT.WHITE, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    df.tft.text((left_margin, h), "Start cutting", TFT.WHITE, sysfont, 1, nowrap=True)
-    h += sysfont["Height"]
-    
-def menuPointer(minValue, maxValue):
-    left_margin = 5
-    if itr.encoderPos <= maxValue and itr.encoderPos >= minValue:
-        h = 5 + itr.encoderPos*sysfont["Height"]
-        df.tft.text((left_margin, h), "*", TFT.RED, sysfont, 1, nowrap=True) 
-        df.tft.text((left_margin, h-sysfont["Height"]), "*", TFT.BLACK, sysfont, 1, nowrap=True)
-        df.tft.text((left_margin, h+sysfont["Height"]), "*", TFT.BLACK, sysfont, 1, nowrap=True)
-    elif itr.encoderPos >= maxValue:
-        itr.encoderPos = maxValue
-    elif itr.encoderPos <= minValue:
-        itr.encoderPos = minValue
-    #print('Encoder @ position ',itr.encoderPos)
-    
+class spawnMenu:
+    def __init__(self, menuName, menuMin, menuMax, *arg):
+        self.menuName = menuName
+        self.menuMin = menuMin
+        self.menuMax = menuMax
+        self.arg = arg
+        
+    def menuDisplay(self):
+        global left_margin
+        df.tft.fill(TFT.BLACK)
+        h = 5
+        for item in self.arg:
+            print(self.arg)
+            df.tft.text((left_margin, h), item, TFT.WHITE, sysfont, 1, nowrap=True)
+            h += sysfont["Height"]
+
+    def menuPointer(self):
+        global left_margin
+        margin = left_margin -10
+        if itr.encoderPos <= self.menuMax and itr.encoderPos >= self.menuMin:
+            h = 5 + itr.encoderPos*sysfont["Height"]
+            df.tft.text((margin, h), "*", TFT.RED, sysfont, 1, nowrap=True) 
+            df.tft.text((margin, h-sysfont["Height"]), "*", TFT.BLACK, sysfont, 1, nowrap=True)
+            df.tft.text((margin, h+sysfont["Height"]), "*", TFT.BLACK, sysfont, 1, nowrap=True)
+        elif itr.encoderPos >= self.menuMax:
+            itr.encoderPos = self.menuMax
+        elif itr.encoderPos <= self.menuMin:
+            itr.encoderPos = self.menuMin
+
 def rotaryMenu():
     itr.oldEncPos
     if  itr.oldEncPos != itr.encoderPos:
